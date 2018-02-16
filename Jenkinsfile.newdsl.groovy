08258d6b35217eeb8517063ceda644f482fd97c4
Justin Davis <justinndavis@gmail.com>
2018-02-16 19:06:18 +0000

    Jenkins DSL stuff

--- Jenkinsfile.newdsl.groovy
+++ Jenkinsfile.newdsl.groovy
@@ -10,6 +10,17 @@ pipeline {
         sh "mvn -U -B -s settings.xml install sonar:sonar deploy -Dsonar.login=ce60f9d40cba6f25ab731fbc2384e1f691a11c48"
       }
     }
+
+    stages {
+      stage('Sonar Analysis') {
+        steps {
+          echo "Doing the Sonar Analysis"
+          sh "mvn sonar:sonar \\\n" +
+                  "  -Dsonar.host.url=https://sonarqube-cicd.apps.ocp.datr.eu \\\n" +
+                  "  -Dsonar.login=ce60f9d40cba6f25ab731fbc2384e1f691a11c48"
+        }
+      }
+    }
 //    stage('Openshift Build') {
 //      steps{
 //        echo "Doing the Openshift Build"
