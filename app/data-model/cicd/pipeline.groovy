430a53526e7caf67a54b9fe0980ad1ae066b1acc
Justin Davis <justinndavis@gmail.com>
2018-03-29 14:55:10 +0100

    pipeline updates

---
+++ app/data-model/cicd/pipeline.groovy
@@ -0,0 +1,65 @@
+#!groovy
+
+node('maven') {
+
+    stage('Checkout Source') {
+        git url: "${git_url}"
+    }
+
+    dir('app/data-model') {
+
+        def dev_project  = "${org}-dev"
+        def prod_project = "${org}-prod"
+        def app_url_dev  = "http://${app_name}.${dev_project}.svc:8080"
+        def groupId      = getGroupIdFromPom("pom.xml")
+        def artifactId   = getArtifactIdFromPom("pom.xml")
+        def version      = getVersionFromPom("pom.xml")
+        def packaging    = getPackagingFromPom("pom.xml")
+        def sonar_url    = "http://sonarqube.cicd.svc:9000"
+        def nexus_url    = "http://nexus.cicd.svc:8081/repository/maven-snapshots"
+
+        stage('Build jar') {
+            echo "Building version"
+            sh "mvn -U -B -q -s ../settings.xml clean package -DskipTests"
+        }
+
+        // Using Maven run the unit tests
+        stage('Unit Tests') {
+            echo "Running Unit Tests"
+            sh "mvn -U -B -q -s ../settings.xml test"
+        }
+
+        // Using Maven call SonarQube for Code Analysis
+        stage('Code Analysis') {
+            echo "Running Code Analysis"
+            sh "mvn -U -B -q -s ../settings.xml sonar:sonar -DskipTests -Dsonar.host.url=${sonar_url}"
+        }
+
+        // Publish the built war file to Nexus
+        stage('Publish to Nexus') {
+            echo "Publish to Nexus"
+            sh "mvn -U -B -q -s ../settings.xml deploy -DskipTests -DaltDeploymentRepository=nexus::default::${nexus_url}"
+        }
+
+    }
+}
+
+// Convenience Functions to read variables from the pom.xml
+// Do not change anything below this line.
+// --------------------------------------------------------
+def getVersionFromPom(pom) {
+    def matcher = readFile(pom) =~ '<version>(.+)</version>'
+    matcher ? matcher[0][1] : null
+}
+def getGroupIdFromPom(pom) {
+    def matcher = readFile(pom) =~ '<groupId>(.+)</groupId>'
+    matcher ? matcher[0][1] : null
+}
+def getArtifactIdFromPom(pom) {
+    def matcher = readFile(pom) =~ '<artifactId>(.+)</artifactId>'
+    matcher ? matcher[0][1] : null
+}
+def getPackagingFromPom(pom) {
+    def matcher = readFile(pom) =~ '<packaging>(.+)</packaging>'
+    matcher ? matcher[0][1] : null
+}
---
+++ app/data-model/cicd/setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+APP=inventory
+S2I_IMAGE=redhat-openjdk18-openshift:1.2
+
+. ../../env.sh
+
+#setup Jenkins Jobs
+JENKINS_USER=justin-admin
+JENKINS_TOKEN=8d2dd9709fb7ebd87d77bf9d2ef4a1f1
+JENKINS=jenkins-cicd.apps.ocp.datr.eu
+
+CRUMB_JSON=$(curl -s "https://${JENKINS_USER}:${JENKINS_TOKEN}@${JENKINS}/crumbIssuer/api/json")
+
+echo CRUMB_JSON=$CRUMB_JSON
+CRUMB=$(echo $CRUMB_JSON | jq -r .crumb)
+echo CRUMB=$CRUMB
+
+curl -v -H "Content-Type: text/xml" \
+  --user ${JENKINS_USER}:${JENKINS_TOKEN} \
+  -H Jenkins-Crumb:${CRUMB} \
+  -X POST https://${JENKINS}/job/amazin-data-model/doDelete
+
+sleep 5
+
+curl -v -H "Content-Type: text/xml" \
+  --user ${JENKINS_USER}:${JENKINS_TOKEN} \
+  -H Jenkins-Crumb:${CRUMB} \
+  --data-binary @config.xml \
+  -X POST https://${JENKINS}/createItem?name=amazin-data-model
+
