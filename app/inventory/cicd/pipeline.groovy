b822dc509b84cba0c977131a903be59b2de114c5
Justin Davis <justinndavis@gmail.com>
2018-03-29 14:35:23 +0100

    pipeline updates

--- app/inventory/cicd/pipeline.groovy
+++ app/inventory/cicd/pipeline.groovy
@@ -25,6 +25,7 @@ node('maven') {
         echo "Building version"
         sh "mvn -U -B -q -s ../settings.xml clean package -DskipTests"
     }
+}
 
 //    // Using Maven run the unit tests
 //    stage('Unit Tests') {
@@ -90,24 +91,24 @@ node('maven') {
 //        }
 //     }
 
-}
 
-// Convenience Functions to read variables from the pom.xml
-// Do not change anything below this line.
-// --------------------------------------------------------
-def getVersionFromPom(pom) {
-    def matcher = readFile(pom) =~ '<version>(.+)</version>'
-    matcher ? matcher[0][1] : null
-}
-def getGroupIdFromPom(pom) {
-    def matcher = readFile(pom) =~ '<groupId>(.+)</groupId>'
-    matcher ? matcher[0][1] : null
-}
-def getArtifactIdFromPom(pom) {
-    def matcher = readFile(pom) =~ '<artifactId>(.+)</artifactId>'
-    matcher ? matcher[0][1] : null
-}
-def getPackagingFromPom(pom) {
-    def matcher = readFile(pom) =~ '<packaging>(.+)</packaging>'
-    matcher ? matcher[0][1] : null
-}
+
+//// Convenience Functions to read variables from the pom.xml
+//// Do not change anything below this line.
+//// --------------------------------------------------------
+//def getVersionFromPom(pom) {
+//    def matcher = readFile(pom) =~ '<version>(.+)</version>'
+//    matcher ? matcher[0][1] : null
+//}
+//def getGroupIdFromPom(pom) {
+//    def matcher = readFile(pom) =~ '<groupId>(.+)</groupId>'
+//    matcher ? matcher[0][1] : null
+//}
+//def getArtifactIdFromPom(pom) {
+//    def matcher = readFile(pom) =~ '<artifactId>(.+)</artifactId>'
+//    matcher ? matcher[0][1] : null
+//}
+//def getPackagingFromPom(pom) {
+//    def matcher = readFile(pom) =~ '<packaging>(.+)</packaging>'
+//    matcher ? matcher[0][1] : null
+//}
