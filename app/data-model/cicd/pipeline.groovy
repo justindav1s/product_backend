4ccc4524bf2cc17ae0b5a51db745bc17b4f22c48
Justin Davis <justinndavis@gmail.com>
2018-03-29 17:09:34 +0100

    pipeline updates

--- app/data-model/cicd/pipeline.groovy
+++ app/data-model/cicd/pipeline.groovy
@@ -10,13 +10,11 @@ node('maven') {
 
         def dev_project  = "${org}-dev"
         def prod_project = "${org}-prod"
-        def app_url_dev  = "http://${app_name}.${dev_project}.svc:8080"
         def groupId      = getGroupIdFromPom("pom.xml")
         def artifactId   = getArtifactIdFromPom("pom.xml")
         def version      = getVersionFromPom("pom.xml")
         def packaging    = getPackagingFromPom("pom.xml")
         def sonar_url    = "http://sonarqube.cicd.svc:9000"
-        def nexus_url    = "http://nexus.cicd.svc:8081/repository/maven-snapshots"
 
         stage('Build jar') {
             echo "Building version : ${version}"
@@ -26,19 +24,19 @@ node('maven') {
         // Using Maven run the unit tests
         stage('Unit Tests') {
             echo "Running Unit Tests"
-            sh "mvn -U -B -s ../settings.xml test"
+            sh "mvn -U -B -q -s ../settings.xml test"
         }
 
         // Using Maven call SonarQube for Code Analysis
         stage('Code Analysis') {
             echo "Running Code Analysis"
-            sh "mvn -U -B -s ../settings.xml sonar:sonar -Dsonar.host.url=${sonar_url}"
+            sh "mvn -U -B -q -s ../settings.xml sonar:sonar -Dsonar.host.url=${sonar_url}"
         }
 
         // Publish the built war file to Nexus
         stage('Publish to Nexus') {
             echo "Publish to Nexus"
-            sh "mvn -U -B -s ../settings.xml deploy -DskipTests"
+            sh "mvn -U -B -q -s ../settings.xml deploy -DskipTests"
         }
 
     }
