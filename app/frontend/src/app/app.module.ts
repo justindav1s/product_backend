ccc5391719a05418b03fb69712aa8cae0136769b
Justin Davis <justinndavis@gmail.com>
2018-03-31 14:11:35 +0100

    angular frontend code

--- app/frontend/src/app/app.module.ts
+++ app/frontend/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { AppRoutingModule }     from './app-routing.module';
+import { InventoryService }          from './inventory.service';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [InventoryService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
