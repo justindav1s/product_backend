fa72108ab2918468e6d60f1d82fee261612d9a04
Justin Davis <justinndavis@gmail.com>
2018-04-05 17:41:31 +0100

    new user service

--- app/basket/src/main/java/org/jnd/user/repositories/BasketRepository.java
+++ app/basket/src/main/java/org/jnd/user/repositories/BasketRepository.java
@@ -1,6 +1,9 @@
 package org.jnd.user.repositories;
 
 import org.jnd.microservices.model.Basket;
+import org.jnd.user.controller.BasketController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -13,19 +16,29 @@ import java.util.Set;
 @Component("BasketRepository")
 public class BasketRepository {
 
+    private static final Logger log = LoggerFactory.getLogger(BasketRepository.class);
+
     private HashMap<Integer, Basket> baskets = new HashMap<Integer, Basket>();
     private HashMap<String, Integer> lookups = new HashMap<String, Integer>();
 
     public Basket get(Integer basketId)   {
-        return baskets.get(basketId);
+        log.debug("BasketRepository get by id :"+basketId);
+        Basket basket = baskets.get(basketId);
+        log.debug("BasketRepository get by id :"+basket+" basket : "+basket);
+        return basket;
     }
 
     public Basket get(String username)   {
+        log.debug("BasketRepository get by username :"+username);
         Integer basketId = lookups.get(username);
-        return baskets.get(basketId);
+        log.debug("BasketRepository get by username :"+username+" basketid : "+basketId);
+        Basket basket = baskets.get(basketId);
+        log.debug("BasketRepository get by username :"+username+" basket : "+basket);
+        return basket;
     }
 
     public void add(Basket basket)   {
+        log.debug("BasketRepository add basket : "+basket);
         lookups.put(basket.getUserId(), basket.getId());
         baskets.put(basket.getId(), basket);
     }
