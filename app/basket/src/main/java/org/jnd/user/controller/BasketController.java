ab23ccfd537c084c3040f9f0a0ff49258feb5f92
Justin Davis <justinndavis@gmail.com>
2018-04-05 19:14:46 +0100

    basket fixes

--- app/basket/src/main/java/org/jnd/user/controller/BasketController.java
+++ app/basket/src/main/java/org/jnd/user/controller/BasketController.java
@@ -78,7 +78,7 @@ public class BasketController {
         log.debug("Basket #"+basketId+" Add Product#"+productId);
 
         Product product = productrepository.getProduct(productId);
-        Basket basket = basketRepository.get(Integer.toString(basketId));
+        Basket basket = basketRepository.get(basketId);
         if (basket.getProducts() != null) {
             basket.getProducts().add(product);
         }
