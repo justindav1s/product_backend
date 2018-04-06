9a1c070dee36de21e8d62c4c7052765588c1cd27
Justin Davis <justinndavis@gmail.com>
2018-04-06 13:43:30 +0100

    cicd for the frontend

--- app/frontend/ocp/setup.sh
+++ app/frontend/ocp/setup.sh
@@ -18,6 +18,6 @@ oc delete configmap ${APP}-config -n ${DEV_PROJECT}
 echo Setting up ${APP} for ${DEV_PROJECT}
 oc new-build --binary=true --labels=app=${APP} --name=${APP} ${S2I_IMAGE} -n ${DEV_PROJECT}
 oc new-app -f ./${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
-oc expose dc ${APP} --port 80 -n ${DEV_PROJECT}
+oc expose dc ${APP} --port 8080 -n ${DEV_PROJECT}
 oc expose svc ${APP} -l app=${APP} -n ${DEV_PROJECT}
 
