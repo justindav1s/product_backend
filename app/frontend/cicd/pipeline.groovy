5ddf4ebbbaf3886f61225e30f7d5cd3a857e07ec
Justin Davis <justinndavis@gmail.com>
2018-04-06 10:04:42 +0100

    cicd for the frontend

---
+++ app/frontend/cicd/pipeline.groovy
@@ -0,0 +1,47 @@
+#!groovy
+
+node('node') {
+
+    stage('Checkout Source') {
+        git url: "${git_url}"
+    }
+
+    dir('app/{{app-name}}') {
+
+        def dev_project  = "${org}-dev"
+        def prod_project = "${org}-prod"
+        def sonar_url    = "http://sonarqube.cicd.svc:9000"
+
+        stage('npm') {
+          sh 'npm install'
+        }
+
+        stage('Angular build') {
+          sh 'ng build'
+        }
+
+      //Build the OpenShift Image in OpenShift and tag it.
+      stage('Build and Tag OpenShift Image') {
+        echo "Building OpenShift container image ${app_name}:${devTag}"
+        echo "Project : ${dev_project}"
+        echo "App : ${app_name}"
+
+        sh "oc start-build ${app_name} --follow --from-dir=dist -n ${dev_project}"
+        openshiftVerifyBuild apiURL: '', authToken: '', bldCfg: app_name, checkForTriggeredDeployments: 'false', namespace: dev_project, verbose: 'false', waitTime: ''
+        openshiftTag alias: 'false', apiURL: '', authToken: '', destStream: app_name, destTag: devTag, destinationAuthToken: '', destinationNamespace: dev_project, namespace: dev_project, srcStream: app_name, srcTag: 'latest', verbose: 'false'
+      }
+
+      // Deploy the built image to the Development Environment.
+      stage('Deploy to Dev') {
+        echo "Deploying container image to Development Project"
+        echo "Project : ${dev_project}"
+        echo "App : ${app_name}"
+        echo "Dev Tag : ${devTag}"
+
+        sh "oc set image dc/${app_name} ${app_name}=docker-registry.default.svc:5000/${dev_project}/${app_name}:${devTag} -n ${dev_project}"
+
+        openshiftDeploy apiURL: '', authToken: '', depCfg: app_name, namespace: dev_project, verbose: 'false', waitTime: '180', waitUnit: 'sec'
+        openshiftVerifyDeployment apiURL: '', authToken: '', depCfg: app_name, namespace: dev_project, replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '180', waitUnit: 'sec'
+      }
+    }
+}
