be1c478f289a38ba60b9db25a59133ab84725ce8
Justin Davis <justinndavis@gmail.com>
2018-04-10 17:47:15 +0100

    integration tests

---
+++ app/inventory/src/main/java/org/jnd/product/repository/ProductCache.java
@@ -0,0 +1,48 @@
+package org.jnd.product.repository;
+
+import org.jnd.microservices.model.Product;
+import org.jnd.microservices.model.ProductType;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component("ProductCache")
+public class ProductCache {
+
+    public HashMap<String, Product> getFood()   {
+
+        HashMap<String, Product> food = new HashMap<String, Product>();
+        food.put("1", new Product("1", "marmalade", ProductType.FOOD, 1.29f));
+        food.put("2", new Product("2", "milk", ProductType.FOOD, 0.83f));
+        food.put("3", new Product("3", "chocolate", ProductType.FOOD, 0.79f));
+        food.put("4", new Product("4", "bread", ProductType.FOOD, 1.09f));
+        food.put("5", new Product("5", "broccoli", ProductType.FOOD, 0.99f));
+        food.put("6", new Product("6", "chicken", ProductType.FOOD, 3.99f));
+        food.put("7", new Product("7", "coffee", ProductType.FOOD, 3.39f));
+        food.put("8", new Product("8", "tea", ProductType.FOOD, 1.29f));
+        food.put("9", new Product("9", "biscuits", ProductType.FOOD, 0.79f));
+        food.put("10", new Product("10", "cake", ProductType.FOOD, 2.79f));
+
+        return food;
+    }
+
+    public HashMap<String, Product> getClothes() {
+
+        HashMap<String, Product> clothes = new HashMap<String, Product>();
+        clothes.put("11", new Product("11", "socks", ProductType.CLOTHES, 3.39f));
+        clothes.put("12", new Product("12", "jacket", ProductType.CLOTHES, 49.99f));
+        clothes.put("13", new Product("13", "shoes", ProductType.CLOTHES, 59.99f));
+        clothes.put("14", new Product("14", "hat", ProductType.CLOTHES, 20.99f));
+        return clothes;
+    }
+
+    public HashMap<String, Product> getGadgets() {
+
+        HashMap<String, Product> gadgets = new HashMap<String, Product>();
+        gadgets.put("11", new Product("11", "socks", ProductType.CLOTHES, 3.39f));
+        gadgets.put("12", new Product("12", "jacket", ProductType.CLOTHES, 49.99f));
+        gadgets.put("13", new Product("13", "shoes", ProductType.CLOTHES, 59.99f));
+        gadgets.put("14", new Product("14", "hat", ProductType.CLOTHES, 20.99f));
+        return gadgets;
+    }
+}
--- app/inventory/src/main/java/org/jnd/product/repository/v1/ProductRepository.java
+++ app/inventory/src/main/java/org/jnd/product/repository/v1/ProductRepository.java
@@ -4,6 +4,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jnd.microservices.model.Product;
 import org.jnd.microservices.model.ProductType;
+import org.jnd.product.repository.ProductCache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,9 @@ public class ProductRepository implements org.jnd.product.repository.ProductRepo
     private HashMap<String, Product> products = new HashMap<String, Product>();
     private ArrayList<String> types = new ArrayList<String>();
 
+    @Autowired
+    private ProductCache cache;
+
     @PostConstruct
     public void init()  {
 
@@ -30,17 +35,7 @@ public class ProductRepository implements org.jnd.product.repository.ProductRepo
         }
 
         if(products.size() == 0) {
-
-            products.put("1", new Product("1", "marmalade", ProductType.FOOD, 1.29f));
-            products.put("2", new Product("2", "milk", ProductType.FOOD, 0.83f));
-            products.put("3", new Product("3", "chocolate", ProductType.FOOD, 0.79f));
-            products.put("4", new Product("4", "bread", ProductType.FOOD, 1.09f));
-            products.put("5", new Product("5", "broccoli", ProductType.FOOD, 0.99f));
-            products.put("6", new Product("6", "chicken", ProductType.FOOD, 3.99f));
-            products.put("7", new Product("7", "coffee", ProductType.FOOD, 3.39f));
-            products.put("8", new Product("8", "tea", ProductType.FOOD, 1.29f));
-            products.put("9", new Product("9", "biscuits", ProductType.FOOD, 0.79f));
-            products.put("10", new Product("10", "cake", ProductType.FOOD, 2.79f));
+            products.putAll(cache.getFood());
         }
 
     }
