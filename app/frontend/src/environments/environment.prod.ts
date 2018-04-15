3e1b190c1146b179642ef681a113780fc1b0d4be
Justin Davis <justinndavis@gmail.com>
2018-04-15 17:30:05 +0100

    istio stuff

--- app/frontend/src/environments/environment.prod.ts
+++ app/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  inventory_backend: "api.amazin.apps.ocp.datr.eu",
-  basket_backend: "api.amazin.apps.ocp.datr.eu",
-  user_backend: "api.amazin.apps.ocp.datr.eu"
+  inventory_backend: "api-gateway.amazin.apps.ocp.datr.eu",
+  basket_backend: "api-gateway.amazin.apps.ocp.datr.eu",
+  user_backend: "api-gateway.amazin.apps.ocp.datr.eu"
 };
