bce3dd45be35cb6acfb2ce5c748b9cd1e1267c5f
Justin Davis <justinndavis@gmail.com>
2018-04-16 17:27:25 +0100

    istio stuff

--- istio/routing_alphav2/create-route-all.sh
+++ istio/routing_alphav2/create-route-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-istioctl delete routerules inventory-default inventory-lb
+istioctl delete routerules basket-lb inventory-lb user-lb
 
 istioctl create -f all-routing-rule.yaml
 
