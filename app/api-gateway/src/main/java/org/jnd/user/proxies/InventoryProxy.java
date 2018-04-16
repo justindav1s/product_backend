bc282eea76178d8e1285ad75584caac1e876043f
Justin Davis <justinndavis@gmail.com>
2018-04-16 15:22:11 +0100

    istio stuff

--- app/api-gateway/src/main/java/org/jnd/user/proxies/InventoryProxy.java
+++ app/api-gateway/src/main/java/org/jnd/user/proxies/InventoryProxy.java
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory;
 import org.jnd.microservices.model.Product;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -59,13 +60,16 @@ public class InventoryProxy {
 
     public List<Product> getProductsofType(String type) {
         List<Product> products = restTemplate.getForObject("http://"+inventory_host+"/products/type/"+type, List.class);
+        ResponseEntity<List> response = restTemplate.getForEntity("http://"+inventory_host+"/products/type/"+type, List.class);
 
-        log.debug("Product Response : "+products);
+        log.debug("AFTER B3Headers");
+        getB3Headers(response.getHeaders());
+        log.debug("Product Response : "+response.getBody());
 
         if (products == null)
             throw new RuntimeException();
 
-        return products;
+        return response.getBody();
     }
 
     public List<String> getProductTypes() {
@@ -77,5 +81,27 @@ public class InventoryProxy {
             throw new RuntimeException();
 
         return products;
+    }
+
+    public HttpHeaders getB3Headers(HttpHeaders headers)   {
+        HttpHeaders b3Headers = new HttpHeaders();
+
+        log.debug("x-request-id : " + headers.get("x-request-id"));
+        log.debug("x-b3-traceid : " + headers.get("x-b3-traceid"));
+        log.debug("x-b3-spanid : " + headers.get("x-b3-spanid"));
+        log.debug("x-b3-parentspanid : " + headers.get("x-b3-parentspanid"));
+        log.debug("x-b3-sampled : " + headers.get("x-b3-sampled"));
+        log.debug("x-b3-flags : " + headers.get("x-b3-flags"));
+        log.debug("x-ot-span-context : " + headers.get("x-ot-span-context"));
+
+        b3Headers.put("x-request-id", headers.get("x-request-id"));
+        b3Headers.put("x-b3-traceid", headers.get("x-b3-traceid"));
+        b3Headers.put("x-b3-spanid", headers.get("x-b3-spanid"));
+        b3Headers.put("x-b3-parentspanid", headers.get("x-b3-parentspanid"));
+        b3Headers.put("x-b3-sampled", headers.get("x-b3-sampled"));
+        b3Headers.put("x-b3-flags", headers.get("x-b3-flags"));
+        b3Headers.put("x-ot-span-context", headers.get("x-ot-span-context"));
+
+        return b3Headers;
     }
 }
