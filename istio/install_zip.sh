f97a2a549155d364da0b8d89b11e2537eb271e6c
Justin Davis <justinndavis@gmail.com>
2018-04-18 16:51:46 +0100

    istio stuff

---
+++ istio/install_zip.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+APP=basket
+
+. ../app/env.sh
+
+oc login https://${IP}:8443 -u $USER
+
+cd ~/istio/install/ansible/
+
+ansible-playbook main.yml -e '{"istio": {"release_tag_name": "0.7.1", "delete_resources": true, "addon": ["grafana", "prometheus", "zipkin", "servicegraph"]}}'
+
+cd -
