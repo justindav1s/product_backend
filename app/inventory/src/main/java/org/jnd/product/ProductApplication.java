cce223dcac1ebf494a6edf6400b3a6f1a3df994f
Justin Davis <justinndavis@gmail.com>
2018-04-19 10:25:57 +0100

    zuul

--- app/inventory/src/main/java/org/jnd/product/ProductApplication.java
+++ app/inventory/src/main/java/org/jnd/product/ProductApplication.java
@@ -1,9 +1,9 @@
 package org.jnd.product;
 
 
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
-import brave.sampler.Sampler;
+//import brave.Tracing;
+//import brave.opentracing.BraveTracer;
+//import brave.sampler.Sampler;
 import io.jaegertracing.Tracer.Builder;
 import io.jaegertracing.metrics.*;
 import io.jaegertracing.propagation.B3TextMapCodec;
@@ -20,10 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import zipkin.Span;
-import zipkin.reporter.AsyncReporter;
-import zipkin.reporter.Encoding;
-import zipkin.reporter.okhttp3.OkHttpSender;
+//import zipkin.Span;
+//import zipkin.reporter.AsyncReporter;
+//import zipkin.reporter.Encoding;
+//import zipkin.reporter.okhttp3.OkHttpSender;
 
 
 @ComponentScan
@@ -37,24 +37,24 @@ public class ProductApplication extends SpringBootServletInitializer {
         SpringApplication.run(ProductApplication.class, args);
     }
 
-//    @Bean
-//    public io.opentracing.Tracer jaegerTracer() {
-//
-//        Reporter reporter = new RemoteReporter.Builder().withFlushInterval(10)
-//                .withMaxQueueSize(65000)
-//                .withSender(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"))
-//                .withMetrics(new Metrics(new NoopMetricsFactory()))
-//                .build();
-//
-//        Builder builder = new Builder("inventory")
-//                .withReporter(reporter)
-//                .withSampler(new ConstSampler(true))
-//                .registerInjector(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec())
-//                .registerExtractor(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec());
-//
-//        return builder.build();
-//
-//    }
+    @Bean
+    public io.opentracing.Tracer jaegerTracer() {
+
+        Reporter reporter = new RemoteReporter.Builder().withFlushInterval(10)
+                .withMaxQueueSize(65000)
+                .withSender(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"))
+                .withMetrics(new Metrics(new NoopMetricsFactory()))
+                .build();
+
+        Builder builder = new Builder("inventory")
+                .withReporter(reporter)
+                .withSampler(new ConstSampler(true))
+                .registerInjector(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec())
+                .registerExtractor(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec());
+
+        return builder.build();
+
+    }
 
 //    @Bean
 //    public io.opentracing.Tracer zipkinTracer() {
@@ -64,19 +64,19 @@ public class ProductApplication extends SpringBootServletInitializer {
 //        return BraveTracer.create(braveTracer);
 //    }
 
-    @Bean
-    public io.opentracing.Tracer zipkinTracer() {
-        OkHttpSender okHttpSender = OkHttpSender.builder()
-                .encoding(Encoding.JSON)
-                .endpoint("http://zipkin.istio-system.svc:9411/api/v1/spans")
-                .build();
-        AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
-        Tracing braveTracer = Tracing.newBuilder()
-                .localServiceName("spring-boot")
-                .reporter(reporter)
-                .traceId128Bit(true)
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .build();
-        return BraveTracer.create(braveTracer);
-    }
+//    @Bean
+//    public io.opentracing.Tracer zipkinTracer() {
+//        OkHttpSender okHttpSender = OkHttpSender.builder()
+//                .encoding(Encoding.JSON)
+//                .endpoint("http://zipkin.istio-system.svc:9411/api/v1/spans")
+//                .build();
+//        AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
+//        Tracing braveTracer = Tracing.newBuilder()
+//                .localServiceName("spring-boot")
+//                .reporter(reporter)
+//                .traceId128Bit(true)
+//                .sampler(Sampler.ALWAYS_SAMPLE)
+//                .build();
+//        return BraveTracer.create(braveTracer);
+//    }
 }
---
+++ spring-cloud/zuul/src/main/java/org/jnd/framework/zuul/ZuulApplication.java
@@ -0,0 +1,23 @@
+package org.jnd.framework.zuul;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@Controller
+@EnableZuulProxy
+public class ZuulApplication {
+
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+
+    }
+}
