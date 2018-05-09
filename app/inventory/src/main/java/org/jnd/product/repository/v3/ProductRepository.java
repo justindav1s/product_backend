dd4f107af1e944c6b9745f1bddd7ca4843db2e1a
Justin Davis <justinndavis@gmail.com>
2018-05-09 14:14:32 +0100

    pipeline updates

--- app/inventory/src/main/java/org/jnd/product/repository/v3/ProductRepository.java
+++ app/inventory/src/main/java/org/jnd/product/repository/v3/ProductRepository.java
@@ -2,14 +2,18 @@ package org.jnd.product.repository.v3;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jnd.microservices.model.Product;
 import org.jnd.microservices.model.ProductType;
 import org.jnd.product.repository.ProductCache;
 import org.jnd.product.repository.RepositoryBase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
 
 
 @Component("ProductRepository")
@@ -21,10 +25,16 @@ public class ProductRepository extends RepositoryBase {
     @Autowired
     private ProductCache cache;
 
+    // inject via application.properties
+    @Value("${sleep.time.ms}")
+    private int sleepTime = 0;
+
     @PostConstruct
     public void init()  {
 
         log.debug("Setting up repository");
+        log.debug("Sleep time (ms) : "+sleepTime);
+
         getTypes().add(ProductType.FOOD.toString());
         getTypes().add(ProductType.CLOTHES.toString());
         getTypes().add(ProductType.GADGETS.toString());
@@ -34,6 +44,20 @@ public class ProductRepository extends RepositoryBase {
         getProducts().putAll(cache.getGadgets());
 
 
+    }
+
+    public Map<String, Product> getProducts() {
+
+        log.debug("Sleep time (ms) : "+sleepTime);
+
+        return super.getProducts();
+    }
+
+    public List<String> getTypes() {
+
+        log.debug("Sleep time (ms) : "+sleepTime);
+
+        return super.getTypes();
     }
 
 }
