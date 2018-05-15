20ef5407c9baa47c8a2db9471ee7c02e2f300b5c
Justin Davis <justinndavis@gmail.com>
2018-05-15 17:34:41 +0100

    pipeline updates

--- app/api-gateway/src/main/java/org/jnd/user/controller/GatewayController.java
+++ app/api-gateway/src/main/java/org/jnd/user/controller/GatewayController.java
@@ -126,26 +126,5 @@ public class GatewayController {
         return "OK";
     }
 
-//    public HttpHeaders getB3Headers(HttpHeaders headers)   {
-//        HttpHeaders b3Headers = new HttpHeaders();
-//
-//        log.debug("x-request-id : " + headers.get("x-request-id"));
-//        log.debug("x-b3-traceid : " + headers.get("x-b3-traceid"));
-//        log.debug("x-b3-spanid : " + headers.get("x-b3-spanid"));
-//        log.debug("x-b3-parentspanid : " + headers.get("x-b3-parentspanid"));
-//        log.debug("x-b3-sampled : " + headers.get("x-b3-sampled"));
-//        log.debug("x-b3-flags : " + headers.get("x-b3-flags"));
-//        log.debug("x-ot-span-context : " + headers.get("x-ot-span-context"));
-//
-//        b3Headers.put("x-request-id", headers.get("x-request-id"));
-//        b3Headers.put("x-b3-traceid", headers.get("x-b3-traceid"));
-//        b3Headers.put("x-b3-spanid", headers.get("x-b3-spanid"));
-//        b3Headers.put("x-b3-parentspanid", headers.get("x-b3-parentspanid"));
-//        b3Headers.put("x-b3-sampled", headers.get("x-b3-sampled"));
-//        b3Headers.put("x-b3-flags", headers.get("x-b3-flags"));
-//        b3Headers.put("x-ot-span-context", headers.get("x-ot-span-context"));
-//
-//        return b3Headers;
-//    }
 
 }
