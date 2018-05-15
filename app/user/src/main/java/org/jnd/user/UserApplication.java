382a122f59fd49760ff1bf344d1b9628cd35a700
Justin Davis <justinndavis@gmail.com>
2018-05-15 18:17:41 +0100

    pipeline updates

--- app/user/src/main/java/org/jnd/user/UserApplication.java
+++ app/user/src/main/java/org/jnd/user/UserApplication.java
@@ -9,16 +9,16 @@ package org.jnd.user;
 //import com.uber.jaeger.samplers.ConstSampler;
 //import com.uber.jaeger.senders.HttpSender;
 //import io.opentracing.propagation.Format;
-
-import io.jaegertracing.Tracer;
-import io.jaegertracing.Tracer.Builder;
-import io.jaegertracing.metrics.*;
-import io.jaegertracing.propagation.B3TextMapCodec;
-import io.jaegertracing.reporters.RemoteReporter;
-import io.jaegertracing.reporters.Reporter;
-import io.jaegertracing.samplers.ConstSampler;
-import io.jaegertracing.senders.HttpSender;
-import io.opentracing.propagation.Format;
+//
+//import io.jaegertracing.Tracer;
+//import io.jaegertracing.Tracer.Builder;
+//import io.jaegertracing.metrics.*;
+//import io.jaegertracing.propagation.B3TextMapCodec;
+//import io.jaegertracing.reporters.RemoteReporter;
+//import io.jaegertracing.reporters.Reporter;
+//import io.jaegertracing.samplers.ConstSampler;
+//import io.jaegertracing.senders.HttpSender;
+//import io.opentracing.propagation.Format;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -45,29 +45,29 @@ public class UserApplication extends SpringBootServletInitializer {
         return "OK";
     }
 
-    @Bean
-    public io.opentracing.Tracer jaegerTracer() {
-
-        Builder builder = new Builder("user",
-                new RemoteReporter(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"), 10,
-                        65000, new Metrics(new StatsFactoryImpl(new NullStatsReporter()))),
-                new ConstSampler(true))
-                .registerInjector(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec())
-                .registerExtractor(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec());
-        return builder.build();
-
-//        Reporter reporter = new RemoteReporter.Builder().withFlushInterval(10)
-//                .withMaxQueueSize(65000)
-//                .withSender(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"))
-//                .withMetrics(new Metrics(new NoopMetricsFactory()))
-//                .build();
+//    @Bean
+//    public io.opentracing.Tracer jaegerTracer() {
 //
-//        Builder builder = new Builder("user")
-//                .withReporter(reporter)
-//                .withSampler(new ConstSampler(true))
+//        Builder builder = new Builder("user",
+//                new RemoteReporter(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"), 10,
+//                        65000, new Metrics(new StatsFactoryImpl(new NullStatsReporter()))),
+//                new ConstSampler(true))
 //                .registerInjector(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec())
 //                .registerExtractor(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec());
-//
 //        return builder.build();
-    }
+//
+////        Reporter reporter = new RemoteReporter.Builder().withFlushInterval(10)
+////                .withMaxQueueSize(65000)
+////                .withSender(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"))
+////                .withMetrics(new Metrics(new NoopMetricsFactory()))
+////                .build();
+////
+////        Builder builder = new Builder("user")
+////                .withReporter(reporter)
+////                .withSampler(new ConstSampler(true))
+////                .registerInjector(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec())
+////                .registerExtractor(Format.Builtin.HTTP_HEADERS, new B3TextMapCodec());
+////
+////        return builder.build();
+//    }
 }
