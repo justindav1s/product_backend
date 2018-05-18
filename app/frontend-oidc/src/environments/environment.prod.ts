97e14cd842267999d974b3305d2acafe59949fe9
Justin Davis <justinndavis@gmail.com>
2018-05-18 17:43:55 +0100

    openid-connect stuff

---
+++ app/frontend-oidc/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  inventory_backend: "api-gateway.amazin.apps.ocp.datr.eu",
+  basket_backend: "api-gateway.amazin.apps.ocp.datr.eu",
+  user_backend: "api-gateway.amazin.apps.ocp.datr.eu"
+};
