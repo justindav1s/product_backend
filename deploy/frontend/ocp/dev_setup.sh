9187ea3d5790ba4c02590627f528fcd4786920c3
Justin Davis <justinndavis@gmail.com>
2018-10-19 10:43:09 +0100

    nodjs tweak

--- deploy/frontend/ocp/dev_setup.sh
+++ deploy/frontend/ocp/dev_setup.sh
@@ -16,7 +16,7 @@ oc delete template ${APP}-dev-dc -n ${DEV_PROJECT}
 oc delete configmap ${APP}-config -n ${DEV_PROJECT}
 
 echo Setting up ${APP} for ${DEV_PROJECT}
-oc new-build --binary=true --labels=app=${APP} --name=${APP} ${S2I_IMAGE} -n ${DEV_PROJECT}
+oc new-build --binary=true --strategy=binary --labels=app=${APP} --name=${APP} ${S2I_IMAGE} -n ${DEV_PROJECT}
 oc new-app -f ./${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
 oc expose dc ${APP} --port 8080 -n ${DEV_PROJECT}
 oc expose svc ${APP} -l app=${APP} -n ${DEV_PROJECT}
