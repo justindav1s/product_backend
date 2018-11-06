7534438633661dead8e77486b6a3f48382480e33
Justin Davis <justinndavis@gmail.com>
2018-11-06 11:02:10 +0000

    istio config

--- deploy/frontend/ocp/prd_setup.sh
+++ deploy/frontend/ocp/prd_setup.sh
@@ -18,7 +18,7 @@ oc delete template ${APP}-dev-dc -n ${PROD_PROJECT}
 oc delete configmap ${APP}-config -n ${PROD_PROJECT}
 
 echo Setting up ${APP} for ${PROD_PROJECT}
-oc new-build ${S2I_IMAGE} --binary=true --labels=app=${APP} --name=${APP}  -n ${PROD_PROJECT}
+oc new-build --binary=true --strategy=source --labels=app=${APP} --name=${APP} --image-stream=${S2I_IMAGE} -n ${DEV_PROJECT}
 oc new-app -f ./${APP}-prod-dc.yaml --allow-missing-imagestream-tags=true -n ${PROD_PROJECT}
 oc expose dc ${APP} --port 8080 -n ${PROD_PROJECT}
 
