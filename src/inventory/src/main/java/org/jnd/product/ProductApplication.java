e30c02e8aaeacdb5c87743c5342adb85a8ea840d
justinndavis@gmail.com <S@far123>
2018-11-08 21:42:30 +0000

    update

--- src/inventory/src/main/java/org/jnd/product/ProductApplication.java
+++ src/inventory/src/main/java/org/jnd/product/ProductApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @ComponentScan
