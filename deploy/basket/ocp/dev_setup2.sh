4832e31bf15822cd65d2baad0ce82db552a0796d
justinndavis@gmail.com <S@far123>
2018-11-08 18:29:23 +0000

    update

---
+++ deploy/basket/ocp/dev_setup2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+APP=basket
+S2I_IMAGE=redhat-openjdk18-openshift:1.4
+
+. ../../../env.sh
+
+oc login https://${IP}:8443 -u $USER
+
+oc project ${DEV_PROJECT}
+
+oc delete all -l app=${APP} -n ${DEV_PROJECT}
+oc delete pvc -l app=${APP} -n ${DEV_PROJECT}
+oc delete is,bc,dc,svc,route ${APP} -n ${DEV_PROJECT}
+oc delete template ${APP}-dev-dc ${APP}-dev-template -n ${DEV_PROJECT}
+oc delete configmap ${APP}-config -n ${DEV_PROJECT}
+
+echo Setting up ${APP} for ${DEV_PROJECT}
+oc create -f ${APP}-dev-template.yaml -n ${DEV_PROJECT}
+
