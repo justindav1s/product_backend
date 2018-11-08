7e7eba8e64ce53a71d89cff9803fa29b50403ac2
justinndavis@gmail.com <S@far123>
2018-11-08 21:27:57 +0000

    update

--- deploy/inventory/ocp/dev_setup.sh
+++ deploy/inventory/ocp/dev_setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-
 APP=inventory
-
 S2I_IMAGE=redhat-openjdk18-openshift:1.4
 
 . ../../../env.sh
@@ -13,15 +11,14 @@ oc project ${DEV_PROJECT}
 
 oc delete all -l app=${APP} -n ${DEV_PROJECT}
 oc delete pvc -l app=${APP} -n ${DEV_PROJECT}
-oc delete is,bc,dc,svc,route ${APP} -n ${DEV_PROJECT}
-oc delete template ${APP}-dev-dc -n ${DEV_PROJECT}
+oc delete is,bc,dc,svc,route,sa ${APP} -n ${DEV_PROJECT}
+oc delete template ${APP}-dev-dc ${APP}-dev-template -n ${DEV_PROJECT}
 oc delete configmap ${APP}-config -n ${DEV_PROJECT}
 
 echo Setting up ${APP} for ${DEV_PROJECT}
-oc new-build --binary=true --labels=app=${APP} --name=${APP} ${S2I_IMAGE} -n ${DEV_PROJECT}
-oc new-app -f ../ocp/${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
-oc set volume dc/${APP} --add --name=${APP}-config-vol --mount-path=/etc/${APP} --configmap-name=${APP}-config -n ${DEV_PROJECT}
-oc set triggers dc/${APP} --from-config
-oc expose dc ${APP} --port 8080 -n ${DEV_PROJECT}
-oc expose svc ${APP} -l app=${APP} -n ${DEV_PROJECT}
+oc new-app -f ../../spring-boot-dev-template.yaml \
+    -p APPLICATION_NAME=${APP} \
+    -p BASE_IMAGE_NAMESPACE="openshift" \
+    -p BASE_IMAGE="redhat-openjdk18-openshift:1.4" \
+    -n ${DEV_PROJECT}
 
