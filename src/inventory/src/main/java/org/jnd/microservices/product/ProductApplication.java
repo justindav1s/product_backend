a79bde8844df0661e44a6169f5b34004462c533b
justinndavis@gmail.com <S@far123>
2018-11-09 11:53:06 +0000

    refactoring

--- src/inventory/src/main/java/org/jnd/microservices/product/ProductApplication.java
+++ src/inventory/src/main/java/org/jnd/microservices/product/ProductApplication.java
@@ -1,30 +1,29 @@
-package org.jnd.microservices.user;
-
-
+package org.jnd.microservices.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+
+@ComponentScan
+@SpringBootApplication(scanBasePackages={"org.jnd"})
+@RestController
 @Configuration
 @EnableAutoConfiguration
-@SpringBootApplication
-@RestController
-public class UserApplication extends SpringBootServletInitializer {
+public class ProductApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args);
     }
 
     @RequestMapping(value = "/health", method = RequestMethod.GET)
     public String ping() {
         return "OK";
     }
-
 
 }
