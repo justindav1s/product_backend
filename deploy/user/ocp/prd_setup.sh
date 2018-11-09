eb0411fcb07278934fea5b385286823808f19065
justinndavis@gmail.com <S@far123>
2018-11-09 17:04:30 +0000

    refactoring

--- deploy/user/ocp/prd_setup.sh
+++ deploy/user/ocp/prd_setup.sh
@@ -8,9 +8,10 @@ oc login https://${IP}:8443 -u $USER
 
 oc project ${PROD_PROJECT}
 
-APP_SA=user-sa
+APP_SA=${APP}-sa
 
-oc delete ${APP_SA}
+oc delete sa ${APP_SA}
+oc delete template spring-boot-prd-template
 oc delete deployments -l app=${APP} -n ${PROD_PROJECT}
 oc delete deploymentconfigs -l app=${APP} -n ${PROD_PROJECT}
 oc delete po -l app=${APP} -n ${PROD_PROJECT}
@@ -19,9 +20,12 @@ oc delete svc -l app=${APP} -n ${PROD_PROJECT}
 oc delete bc -l app=${APP} -n ${PROD_PROJECT}
 oc delete routes -l app=${APP} -n ${PROD_PROJECT}
 
-oc apply -f ${APP}-sa-prod.yaml
+oc new-app -f ../../spring-boot-prd-template.yaml \
+    -p APPLICATION_NAME=${APP} \
+    -p APPLICATION_VERSION=0.0.1-SNAPSHOT \
+    -p VERSION_LABEL=v1
 
+sleep 2
+
+oc policy add-role-to-group system:image-puller system:serviceaccounts:${APP_SA} -n ${DEV_PROJECT}
 oc adm policy add-scc-to-user privileged -z ${APP_SA}
-
-oc apply -f ${APP}-istio-prod.yaml
-
