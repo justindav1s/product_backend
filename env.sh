1ef0ac4428dbdc349c34e7c03b8a7f3aad5d090c
Justin Davis <justinndavis@gmail.com>
2018-11-10 14:29:34 +0000

    deployment improvements

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#export IP=192.168.0.91
+export IP=192.168.0.91
 #export IP=ocp.datr.eu
-export IP=local.datr.eu
+#export IP=local.datr.eu
 export USER=justin
 
 export ORG=amazin
@@ -13,6 +13,6 @@ export CICD_PROJECT=cicd
 export CURL="curl -k -v"
 export JENKINS_USER=justin-admin
 export JENKINS_TOKEN=a96aec8d7dd2c9fbec501d96db95ca60
-#export JENKINS=jenkins-cicd.apps.ocp.datr.eu
-export JENKINS=jenkins-cicd.apps.${IP}
+export JENKINS=jenkins-cicd.apps.ocp.datr.eu
+#export JENKINS=jenkins-cicd.apps.${IP}
 
