39c5b78ab034b6d06bad8b7374ebf6fb20f02217
Justin Davis <justinndavis@gmail.com>
2018-11-12 09:02:22 +0000

    deployment improvements

--- deploy_istio_mtls_authn_authz/istio/create-prd-gateway.sh
+++ deploy_istio_mtls_authn_authz/istio/create-prd-gateway.sh
@@ -2,22 +2,25 @@
 
 oc project amazin-prod
 
-oc delete policy default
-oc delete virtualservice amazin-prd
-oc delete virtualservice inventory
-oc delete virtualservice user
-oc delete virtualservice basket
-oc delete virtualservice api-gateway
-oc delete destinationrule inventory
-oc delete destinationrule user
-oc delete destinationrule basket
-oc delete destinationrule api-gateway
-oc delete gateway amazin-gateway-prod
+oc delete rbaconfig --all -n isio-system
+oc delete servicerole --all
+oc delete servicerolebinding --all
+oc delete policy --all
+oc delete virtualservice --all
+oc delete destinationrule --all
+oc delete gateway --all
 
 oc adm policy add-scc-to-user anyuid -z default -n amazin-prod
 oc adm policy add-scc-to-user privileged -z default -n amazin-prod
 
-oc apply -f namespace-policy.yaml -n amazin-prod
+oc apply -f rbac-on.yaml -n istio-system
+
+#Authn config
+oc apply -f authn-policy.yaml -n amazin-prod
+
+#Authz config
+oc apply -f authz-roles.yaml -n amazin-prod
+oc apply -f authz-bindings.yaml -n amazin-prod
 
 oc apply -f amazin-prd-gateway.yaml -n amazin-prod
 oc apply -f amazin-prd-destrules-mtls.yaml -n amazin-prod
