87a2d5e1afe8cdc08d8e563467007ddbcf527b2e
Justin Davis <justinndavis@gmail.com>
2018-11-12 13:57:23 +0000

    cleaningup web app deployment

--- deploy_istio_mtls_default_sa/web/ocp/dev_setup.sh
+++ deploy_istio_mtls_default_sa/web/ocp/dev_setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 APP=web
-S2I_IMAGE=openshift/nginx:1.10
 
 . ../../../env.sh
 
@@ -12,12 +11,9 @@ oc project ${DEV_PROJECT}
 oc delete all -l app=${APP} -n ${DEV_PROJECT}
 oc delete pvc -l app=${APP} -n ${DEV_PROJECT}
 oc delete is,bc,dc,svc,route ${APP} -n ${DEV_PROJECT}
-oc delete template ${APP}-dev-dc -n ${DEV_PROJECT}
+oc delete template ${APP}-dev-dc ${APP}-dev-template -n ${DEV_PROJECT}
 oc delete configmap ${APP}-config -n ${DEV_PROJECT}
 
 echo Setting up ${APP} for ${DEV_PROJECT}
-oc new-build --binary=true --strategy=source --labels=app=${APP} --name=${APP} --image-stream=${S2I_IMAGE} -n ${DEV_PROJECT}
-oc new-app -f ./${APP}-dev-dc.yaml --allow-missing-imagestream-tags=true -n ${DEV_PROJECT}
-oc expose dc ${APP} --port 8080 -n ${DEV_PROJECT}
-oc expose svc ${APP} -l app=${APP} -n ${DEV_PROJECT}
+oc create -f ${APP}-dev-template.yaml -n ${DEV_PROJECT}
 
