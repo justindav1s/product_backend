b02cc471d3010c0196e1281ab1f09c4f29682355
Justin Davis <justinndavis@gmail.com>
2018-11-16 16:13:27 +0000

    xip.io updates

--- cicd_tools/cicd-setup.sh
+++ cicd_tools/cicd-setup.sh
@@ -6,9 +6,9 @@
 oc login https://${IP}:8443 -u $USER
 
 oc delete project $CICD_PROJECT
-oc adm new-project $CICD_PROJECT --node-selector='capability=apps' 2> /dev/null
+oc adm new-project $CICD_PROJECT --node-selector='capability=infra' 2> /dev/null
 while [ $? \> 0 ]; do
     sleep 1
     printf "."
-oc adm new-project $CICD_PROJECT --node-selector='capability=apps' 2> /dev/null
+oc adm new-project $CICD_PROJECT --node-selector='capability=infra' 2> /dev/null
 done
