689067969ff120c82ae0c894a70840ab77eeabd7
Justin Davis <justinndavis@gmail.com>
2018-12-07 15:47:33 +0000

    grafeas

--- cicd_tools/grafeas-service/src/main/java/org/jnd/microservices/grafeas/GrafeasApplication.java
+++ cicd_tools/grafeas-service/src/main/java/org/jnd/microservices/grafeas/GrafeasApplication.java
@@ -1,5 +1,10 @@
 package org.jnd.microservices.grafeas;
 
+import com.squareup.okhttp.OkHttpClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.GrafeasV1Beta1Api;
+import io.swagger.client.model.V1beta1Note;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,8 +23,28 @@ public class GrafeasApplication {
 		SpringApplication.run(GrafeasApplication.class, args);
 	}
 
+	@Value("${grafeas.host:not_found}")
+	private String grafeas = null;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		return "GRAFEAS";
+	}
+
+	@RequestMapping(value = "/go", method = RequestMethod.GET)
+	public String go()	{
+		OkHttpClient httpClient = new OkHttpClient();
+		GrafeasV1Beta1Api apiInstance = new GrafeasV1Beta1Api();
+		apiInstance.getApiClient().setBasePath(grafeas);
+		String parent = "parent_example"; // String |
+		V1beta1Note body = new V1beta1Note(); // ApiNote |
+		try {
+			V1beta1Note result = apiInstance.createNote(parent, body);
+			System.out.println(result);
+		} catch (ApiException e) {
+			System.err.println("Exception when calling GrafeasApi#createNote");
+			e.printStackTrace();
+		}
+
 	}
 }
