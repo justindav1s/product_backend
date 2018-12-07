3c1c60d8b20620751d6d566f562791ce7d282c0e
Justin Davis <justinndavis@gmail.com>
2018-12-07 13:46:37 +0000

    grafeas

---
+++ cicd_tools/grafeas-service/src/main/java/org/jnd/microservices/grafeas/GrafeasApplication.java
@@ -0,0 +1,12 @@
+package org.jnd.microservices.grafeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafeasApplication.class, args);
+	}
+}
