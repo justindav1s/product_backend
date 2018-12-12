7bbca3c6fabc403ed24017ad6183a60d90d01bc0
Justin Davis <justinndavis@gmail.com>
2018-12-12 14:19:10 +0000

    gitlab stuff

--- cicd_pipelines/user/pipeline.groovy
+++ cicd_pipelines/user/pipeline.groovy
@@ -2,7 +2,7 @@
 
 node('maven') {
 
-    def shortCommit;
+    def commitId;
 
     dir ("build")   {
 
@@ -23,7 +23,7 @@ node('maven') {
             def packaging = getPackagingFromPom("pom.xml")
             def sonar_url = "http://sonarqube.cicd.svc:9000"
             def nexus_url = "http://nexus.cicd.svc:8081/repository/maven-snapshots"
-            shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+            commitId = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
 
             stage('Build jar') {
                 echo "Building version : ${version}"
@@ -106,7 +106,7 @@ node('maven') {
     dir("build-metadata") {
 
         stage('manage version data') {
-            manageVersionData(shortCommit, build_tracking_git_url)
+            manageVersionData(app_name, commitId, build_tracking_git_url)
         }
 
     }
@@ -133,10 +133,10 @@ def getPackagingFromPom(pom) {
     matcher ? matcher[0][1] : null
 }
 
-def manageVersionData(commitId, build_tracking_git_url) {
+def manageVersionData(app_name, commitId, build_tracking_git_url) {
     git url: "${build_tracking_git_url}", branch: 'master', credentialsId: '1c0e3c0a-f7bd-444e-918f-69799380d061'
     def workspace = pwd()
-    def versionFileName = "version"
+    def versionFileName = "version."+app_name
     versionFileName = workspace+"/"+versionFileName
     def versiondata = sh(returnStdout: true, script: "cat ${versionFileName} | head -1")
     println "Existing version data : "+versiondata
@@ -151,8 +151,8 @@ def manageVersionData(commitId, build_tracking_git_url) {
     sshagent(['1c0e3c0a-f7bd-444e-918f-69799380d061']) {
         sh ("git config --global user.email \"justinndavis@gmail.com\"")
         sh ("git config --global user.name \"Justin Davis\"")
-        sh ("git add version")
-        sh ("git commit -m \"updating version data to ${newVersionString}\"")
+        sh ("git add ${versionFileName}")
+        sh ("git commit -m \"updating version data in ${versionFileName} to ${newVersionString}\"")
         sh ("git push origin master")
     }
 }
