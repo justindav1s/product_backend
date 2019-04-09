6aa49160b8e87924263b65a714706fd8010b5df3
Justin Davis <justinndavis@gmail.com>
2019-04-09 17:05:47 +0100

    pipeline updates

--- cicd_pipelines/user/pipeline.groovy
+++ cicd_pipelines/user/pipeline.groovy
@@ -78,6 +78,9 @@ node('maven') {
                 echo "Project : ${dev_project}"
                 echo "App : ${app_name}"
                 echo "Dev Tag : ${devTag}"
+
+                sh "oc set triggers dc/${app_name} --remove-all -n ${dev_project}"
+
                 sh "oc set image dc/${app_name} ${app_name}=docker-registry.default.svc:5000/${dev_project}/${app_name}:${commitId} -n ${dev_project}"
                 def ret = sh(script: "oc delete configmap ${app_name}-config --ignore-not-found=true -n ${dev_project}", returnStdout: true)
                 ret = sh(script: "oc create configmap ${app_name}-config --from-file=${config_file} -n ${dev_project}", returnStdout: true)
