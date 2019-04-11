af791a5c102d97e4d8d3f5fe787a90e74e6a32b4
Justin Davis <justinndavis@gmail.com>
2019-04-11 18:04:12 +0100

    quay updates

--- quay/dev-setup.sh
+++ quay/dev-setup.sh
@@ -4,6 +4,9 @@
 . ../env.sh
 
 DEV_PROJECT=quay-dev
+APP=inventory
+VERSION=dev
+S2I_IMAGE=java:8
 
 oc login https://${IP}:8443 -u $USER
 
@@ -21,5 +24,17 @@ oc policy add-role-to-user edit system:serviceaccount:${CICD_PROJECT}:jenkins -n
 oc policy add-role-to-user edit system:serviceaccount:${CICD_PROJECT}:default -n ${DEV_PROJECT}
 oc policy add-role-to-user view --serviceaccount=default -n ${DEV_PROJECT}
 
+oc delete all -l app=${APP} -n ${DEV_PROJECT}
+oc delete pvc -l app=${APP} -n ${DEV_PROJECT}
+oc delete sa ${APP}-sa
+oc delete is,bc,dc,svc,route,sa ${APP} -n ${DEV_PROJECT}
+oc delete template ${APP}-dev-dc ${APP}-dev-template -n ${DEV_PROJECT}
+oc delete configmap ${APP}-config -n ${DEV_PROJECT}
 
-./inventory_setup.sh
+echo Setting up ${APP} for ${DEV_PROJECT}
+oc new-app -f spring-boot-dev-quay-template.yaml \
+    -p PROJECT=${DEV_PROJECT} \
+    -p APPLICATION_NAME=${APP} \
+    -p BASE_IMAGE_NAMESPACE="openshift" \
+    -p BASE_IMAGE=${S2I_IMAGE} \
+    -n ${DEV_PROJECT}
