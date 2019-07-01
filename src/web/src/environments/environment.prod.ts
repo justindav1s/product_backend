17005b15154a567a804e603df348faa09484455d
Justin Davis <justinndavis@gmail.com>
2019-07-01 13:44:25 +0100

    angular 7 upgrade

--- src/web/src/environments/environment.prod.ts
+++ src/web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  inventory_backend: "https://istio-ingressgateway-istio-system.apps.aws.datr.eu",
-  basket_backend: "https://istio-ingressgateway-istio-system.apps.aws.datr.eu",
-  user_backend: "https://istio-ingressgateway-istio-system.apps.aws.datr.eu"
+  inventory_backend: "https://istio-ingressgateway-istio-system.apps.ocp.datr.eu",
+  basket_backend: "https://istio-ingressgateway-istio-system.apps.ocp.datr.eu",
+  user_backend: "https://istio-ingressgateway-istio-system.apps.ocp.datr.eu"
 };
