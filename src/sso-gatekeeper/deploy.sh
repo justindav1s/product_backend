80e243d4a1b12c276596a068e819ca8618945529
Justin Davis <justinndavis@gmail.com>
2019-07-04 12:30:06 +0100

    adding sso support to gateway using spring security config

--- src/sso-gatekeeper/deploy.sh
+++ src/sso-gatekeeper/deploy.sh
@@ -2,21 +2,11 @@
 
 oc login https://ocp.datr.eu:8443 -u justin
 
-APP=gatekeeper
-PROJECT=$APP
-
-oc delete project $PROJECT
-oc new-project $PROJECT 2> /dev/null
-while [ $? \> 0 ]; do
-    sleep 1
-    printf "."
-oc new-project $PROJECT 2> /dev/null
-done
+APP=sso-gatekeeper
+PROJECT=amazin-dev
 
 oc project $PROJECT
 
-oc delete imagestream ${APP}
-oc delete buildconfig ${APP}-docker-build
 oc delete deploymentconfig ${APP}
 oc delete serviceaccounts ${APP}
 oc delete service ${APP}
@@ -24,7 +14,5 @@ oc delete route ${APP}
 
 oc new-app -f gatekeeper-template.yml \
     -p APPLICATION_NAME=${APP} \
-    -p SOURCE_REPOSITORY_URL=https://github.com/justindav1s/openshift-sso.git \
-    -p SOURCE_REPOSITORY_REF=master \
-    -p DOCKERFILE_PATH="gatekeeper" \
+    -p LISTEN_PORT="3000"
 
