edc7a62bc9b416191c7fda4fedf941bea4e1a67d
Justin Davis <justinndavis@gmail.com>
2019-07-04 14:06:57 +0100

    adding sso support to gateway using spring security config

--- src/sso-gatekeeper/deploy.sh
+++ src/sso-gatekeeper/deploy.sh
@@ -11,9 +11,9 @@ oc delete deploymentconfig ${APP}
 oc delete serviceaccounts ${APP}-sa
 oc delete service ${APP}
 oc delete route ${APP}
+oc delete configmap ${APP}-config --ignore-not-found=true -n ${PROJECT}
 
 oc new-app -f gatekeeper-template.yml \
     -p APPLICATION_NAME=${APP}
 
-oc delete configmap ${APP}-config --ignore-not-found=true -n ${PROJECT}
 oc create configmap ${APP}-config --from-file=config/gatekeeper.yaml -n ${PROJECT}
