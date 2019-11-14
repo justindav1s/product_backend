b4813b3fc1915feee5721328a5317d298b318e0b
Justin Davis <justinndavis@gmail.com>
2019-11-14 13:44:03 +0000

    sso stuff

--- src/sso-relay/src/main/java/org/jnd/microservices/sso/controller/SSOController.java
+++ src/sso-relay/src/main/java/org/jnd/microservices/sso/controller/SSOController.java
@@ -3,10 +3,13 @@ package org.jnd.microservices.sso.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @CrossOrigin
@@ -16,12 +19,19 @@ public class SSOController {
 
     private static final Logger log = LoggerFactory.getLogger(SSOController.class);
 
+    @Value( "${sso.server}" )
+    String sso_server;
 
     @RequestMapping(value = "/handle-oauth", method = RequestMethod.GET, produces = "application/json")
-    public String login(@RequestHeader HttpHeaders headers, @RequestParam(name = "code") String code) {
+    public String login(@RequestHeader HttpHeaders headers, HttpServletRequest request, @RequestParam(name = "code") String code) {
 
         log.info("handle-oauth");
+        String uri = request.getRequestURI();
+
+        log.info("uri : "+uri);
         log.info("code : "+code);
+        log.info("sso_server : "+sso_server);
+
 
         for (String key : headers.keySet()) {
             log.info(key+" : "+headers.get(key));
