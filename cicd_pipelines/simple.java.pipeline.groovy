1fedf1ef67e75b95716cc3a2593342bc662e4a9e
Justin Davis <justinndavis@gmail.com>
2019-11-14 10:46:04 +0000

    sso stuff

--- cicd_pipelines/simple.java.pipeline.groovy
+++ cicd_pipelines/simple.java.pipeline.groovy
@@ -33,6 +33,23 @@ node('maven') {
             junit 'target/surefire-reports/*.xml'
         }
 
+        stage('Coverage') {
+            echo "Running Coverage"
+            sh "${mvn} clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dspring.profiles.active=dev"
+        }
+
+        // Using Maven call SonarQube for Code Analysis
+        stage('Code Analysis') {
+            echo "Running Code Analysis"
+            sh "${mvn} sonar:sonar -Dspring.profiles.active=dev -Dsonar.host.url=${sonar_url}"
+        }
+
+        // Publish the built war file to Nexus
+        stage('Publish to Nexus') {
+            echo "Publish to Nexus"
+            sh "${mvn} deploy -DskipTests"
+        }
+
         //Build the OpenShift Image in OpenShift and tag it.
         stage('Build and Tag OpenShift Image') {
             echo "Building OpenShift container image ${app_name}:${devTag}"
