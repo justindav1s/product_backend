dedd4a34357d89942251bcb73590c8c5080d92b3
Justin Davis <justinndavis@gmail.com>
2019-11-14 16:35:24 +0000

    sso stuff

--- src/sso-relay/authorization_code.sh
+++ src/sso-relay/authorization_code.sh
@@ -26,46 +26,46 @@ echo "Keycloak host : $KEYCLOAK"
 #Get Code
 GET_BODY="scope=openid&response_type=code&client_id=${CLIENT}&redirect_uri=${REDIRECT}"
 
-RESPONSE=$(curl -vk -D headers.txt \
+RESPONSE=$(curl -L -vk -D headers.txt \
     -u ${USER}:${USER_PASSWORD} \
     -X GET \
     ${KEYCLOAK}/auth/realms/${REALM}/protocol/openid-connect/auth?${GET_BODY})
 
 echo RESPONSE=$RESPONSE
-LOC=$(grep Location headers.txt)
-#rm -rf headers.txt
-CODE=`echo ${LOC} | awk -F'[=&]' '{print $4}' | tr -cd "[:print:]\n"`
-
-echo "CODE"=${CODE}
-echo ${#CODE}
-
-#Get Token
-POST_BODY="grant_type=${GRANT_TYPE}&redirect_uri=${REDIRECT}&client_id=${CLIENT}&client_secret=${CLIENT_SECRET}&code="
-POST_BODY=${POST_BODY}${CODE}
-echo POST_BODY=${POST_BODY}
-
-RESPONSE=$(curl -vk \
-    -d ${POST_BODY} \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    ${KEYCLOAK}/auth/realms/${REALM}/protocol/openid-connect/token)
-
-echo "RESPONSE"=${RESPONSE}
-ACCESS_TOKEN=$(echo ${RESPONSE} | jq -r .access_token)
-PART2_BASE64=$(echo ${ACCESS_TOKEN} | cut -d"." -f2)
-PART2_BASE64=$(padBase64 ${PART2_BASE64})
-echo "ACCESS TOKEN"
-echo ${PART2_BASE64} | base64 -D | jq .
-
-echo
-ACCESS_TOKEN=$(echo ${RESPONSE} | jq -r .refresh_token)
-PART2_BASE64=$(echo ${ACCESS_TOKEN} | cut -d"." -f2)
-PART2_BASE64=$(padBase64 ${PART2_BASE64})
-echo "REFRESH TOKEN"
-echo ${PART2_BASE64} | base64 -D | jq .
-
-echo
-ACCESS_TOKEN=$(echo ${RESPONSE} | jq -r .id_token)
-PART2_BASE64=$(echo ${ACCESS_TOKEN} | cut -d"." -f2)
-PART2_BASE64=$(padBase64 ${PART2_BASE64})
-echo "ID TOKEN"
-echo ${PART2_BASE64} | base64 -D | jq .
+#LOC=$(grep Location headers.txt)
+##rm -rf headers.txt
+#CODE=`echo ${LOC} | awk -F'[=&]' '{print $4}' | tr -cd "[:print:]\n"`
+#
+#echo "CODE"=${CODE}
+#echo ${#CODE}
+#
+##Get Token
+#POST_BODY="grant_type=${GRANT_TYPE}&redirect_uri=${REDIRECT}&client_id=${CLIENT}&client_secret=${CLIENT_SECRET}&code="
+#POST_BODY=${POST_BODY}${CODE}
+#echo POST_BODY=${POST_BODY}
+#
+#RESPONSE=$(curl -vk \
+#    -d ${POST_BODY} \
+#    -H "Content-Type: application/x-www-form-urlencoded" \
+#    ${KEYCLOAK}/auth/realms/${REALM}/protocol/openid-connect/token)
+#
+#echo "RESPONSE"=${RESPONSE}
+#ACCESS_TOKEN=$(echo ${RESPONSE} | jq -r .access_token)
+#PART2_BASE64=$(echo ${ACCESS_TOKEN} | cut -d"." -f2)
+#PART2_BASE64=$(padBase64 ${PART2_BASE64})
+#echo "ACCESS TOKEN"
+#echo ${PART2_BASE64} | base64 -D | jq .
+#
+#echo
+#ACCESS_TOKEN=$(echo ${RESPONSE} | jq -r .refresh_token)
+#PART2_BASE64=$(echo ${ACCESS_TOKEN} | cut -d"." -f2)
+#PART2_BASE64=$(padBase64 ${PART2_BASE64})
+#echo "REFRESH TOKEN"
+#echo ${PART2_BASE64} | base64 -D | jq .
+#
+#echo
+#ACCESS_TOKEN=$(echo ${RESPONSE} | jq -r .id_token)
+#PART2_BASE64=$(echo ${ACCESS_TOKEN} | cut -d"." -f2)
+#PART2_BASE64=$(padBase64 ${PART2_BASE64})
+#echo "ID TOKEN"
+#echo ${PART2_BASE64} | base64 -D | jq .
