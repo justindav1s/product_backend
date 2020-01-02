6dfbda38e8a1ad9b76b2b6402126e1ea5ad4b538
Justin Davis <jusdavis@redhat.com>
2020-01-02 14:26:41 +0000

    ocp4 upgrade stuff

--- env.sh
+++ env.sh
@@ -13,7 +13,7 @@ export CICD_PROJECT=cicd
 
 export CURL="curl -k -v"
 export JENKINS_USER=justin-admin-edit-view
-export JENKINS_TOKEN=111f15fceb3e03edd67d0a3605cbf4f7b3
+export JENKINS_TOKEN=1187728df978d0c44510ad7a42aaf25afe
 export JENKINS=jenkins-cicd.apps.ocp4.datr.eu
 export JENKINS=jenkins-cicd.apps-crc.testing
 #export JENKINS=jenkins-cicd.apps.${IP}
