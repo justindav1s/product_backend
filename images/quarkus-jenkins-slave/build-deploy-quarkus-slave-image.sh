507dcf1536919d17e1a4f277c5ea6c2a6e058d32
Justin Davis <justinndavis@gmail.com>
2020-04-08 14:35:18 +0100

    resource management

--- images/quarkus-jenkins-slave/build-deploy-quarkus-slave-image.sh
+++ images/quarkus-jenkins-slave/build-deploy-quarkus-slave-image.sh
@@ -3,10 +3,10 @@
 . ../../env.sh
 
 IMAGE=jenkins-slave-quarkus:latest
-REGISTRY_HOST=nexus3-docker-cicd.apps.ocp4.datr.eu:443
-USER-admin
-#REGISTRY_HOST=quay.io/justindav1s
-#USER=justindav1s
+# REGISTRY_HOST=nexus3-docker-cicd.apps.ocp4.datr.eu:443
+# USER-admin
+REGISTRY_HOST=quay.io/justindav1s
+USER=justindav1s
 
 docker build -t $IMAGE .
 docker tag $IMAGE $REGISTRY_HOST/$IMAGE
