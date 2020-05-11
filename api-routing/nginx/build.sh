252d7d59a88e6cd62b785089e064e8456a8bc272
Justin Davis <justinndavis@gmail.com>
2020-05-11 10:17:34 +0100

    nginx experiments

--- api-routing/nginx/build.sh
+++ api-routing/nginx/build.sh
@@ -13,7 +13,7 @@ oc new-project $PROJECT 2> /dev/null
 done
 
 oc create secret docker-registry rh-dockercfg \
-  --docker-server=registry.access.redhat.com \
+  --docker-server=registry.redhat.io \
   --docker-username=${RHDN_USERNAME} \
   --docker-password=${RHDN_PASSWORD} \
   --docker-email=${RHDN_USERNAME} \
@@ -26,12 +26,15 @@ oc create secret docker-registry quayio-dockercfg \
   --docker-email=${QUAYIO_EMAIL} \
   -n $PROJECT
 
-oc new-build https://github.com/justindav1s/microservices-on-openshift \
-    --docker-image="rhel8/nginx-116" \
+oc secrets link builder rh-dockercfg
+oc secrets link builder quayio-dockercfg
+
+oc new-build registry.redhat.io/rhel8/nginx-116~https://github.com/justindav1s/microservices-on-openshift \
     --push-secret="quayio-dockercfg" \
     --to="quay.io/justindav1s/nginx" \
     --context-dir="api-routing/nginx/src" \
     --strategy="Source" \
+    --to-docker=true \
     --allow-missing-images
 
 
