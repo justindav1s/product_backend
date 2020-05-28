6c80b0121af778928396212578e4c5a6ecf3cdff
Justin Davis <justinndavis@gmail.com>
2020-05-28 10:12:43 +0100

    argocd updates

--- cicd_tools/argocd/deploy/deploy.sh
+++ cicd_tools/argocd/deploy/deploy.sh
@@ -2,8 +2,10 @@
 
 PROJECT=argocd
 
-oc delete -f cluster-admins-group.yaml
-oc delete -f cluster-admins-rolebinding.yaml
+oc delete -f argocd-admins-group.yaml
+oc delete -f project1-admins-group.yaml
+oc delete -f project2-admins-group.yaml
+
 oc delete project $PROJECT
 oc new-project $PROJECT 2> /dev/null
 while [ $? \> 0 ]; do
@@ -17,7 +19,7 @@ oc create secret generic github-secret --from-file=ssh-privatekey=${HOME}/.ssh/i
 oc apply -f operatorgroup.yaml
 oc apply -f subscription.yaml
 oc apply -f argocd_instance.yaml
-#oc apply -f argocd_simple.yaml
-oc apply -f cluster-admins-group.yaml
-oc apply -f cluster-admins-rolebinding.yaml
+oc apply -f argocd-admins-group.yaml
+oc apply -f project1-admins-group.yaml
+oc apply -f project2-admins-group.yaml
 oc apply -f product-plain-yaml-app.yaml
