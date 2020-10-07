b7c840dd077352443f010699f1f7004473d1e34a
Justin Davis <justinndavis@gmail.com>
2020-10-07 13:42:07 +0100

    fix

--- src/inventory/src/main/java/org/jnd/microservices/product/repository/ProductCache.java
+++ src/inventory/src/main/java/org/jnd/microservices/product/repository/ProductCache.java
@@ -6,6 +6,8 @@ import org.jnd.microservices.model.ProductType;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 @Component("ProductCache")
 public class ProductCache {
 
