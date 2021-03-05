5bdbd7cb3db38a68280590da3bfd29e1974d1989
Justin Davis <justinndavis@gmail.com>
2021-03-05 18:00:03 +0000

    fix

--- src/user/src/main/java/org/jnd/microservices/user/UserApplication.java
+++ src/user/src/main/java/org/jnd/microservices/user/UserApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @PropertySources({
